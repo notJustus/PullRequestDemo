698583fa035837ad57501641f1906ede0ee8674f
notJustus <justus.beck@gmx.de>
2025-11-24 11:24:57 +0100

    init

---
+++ PullReqDemo/PullReqDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PullReqDemo
+//
+//  Created by anonymous on 24.11.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PullReqDemo/PullReqDemo/PullReqDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PullReqDemoApp.swift
+//  PullReqDemo
+//
+//  Created by anonymous on 24.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct PullReqDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
