6f0e557c3ab40db13d2716001d9b6dd9bedaa07e
notJustus <justus.beck@gmx.de>
2025-11-24 11:30:20 +0100

    first commit from Justus

---
+++ PullReqDemo/PullReqDemo/NavigatorView.swift
@@ -0,0 +1,19 @@
+//
+//  NavigatorView.swift
+//  PullReqDemo
+//
+//  Created by anonymous on 24.11.2025.
+//
+
+import SwiftUI
+
+struct NavigatorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("To be merged with main")
+    }
+}
+
+#Preview {
+    NavigatorView()
+}
